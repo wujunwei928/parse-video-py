ccb3e0bfea296b68b25f34bb0da778c3f146b394
wujunwei <1399952803@qq.com>
2024-04-28 15:55:31 +0800

    修复快手视频解析问题; 快手支持解析图集

--- parser/kuaishou.py
+++ parser/kuaishou.py
@@ -1,6 +1,3 @@
-import json
-import re
-
 import fake_useragent
 import httpx
 
@@ -13,41 +10,80 @@ class KuaiShou(BaseParser):
     """
 
     async def parse_share_url(self, share_url: str) -> VideoInfo:
-        headers = {
-            "User-Agent": fake_useragent.UserAgent(os=["windows"]).random,
-            "Referer": "https://v.kuaishou.com/",
+        user_agent = fake_useragent.UserAgent(os=["ios"]).random
+
+        # 获取跳转前的信息, 从中获取视频ID, cookie
+        async with httpx.AsyncClient(follow_redirects=False) as client:
+            redirect_response = await client.get(
+                share_url,
+                headers={
+                    "User-Agent": user_agent,
+                    "Referer": "https://v.kuaishou.com/",
+                },
+            )
+
+        redirect_url = redirect_response.headers.get("Location", "")
+        if not redirect_url:
+            raise Exception("failed to get parse video ID from share URL")
+
+        # redirect_response.cookies 直接用于下面的请求会触发反爬虫验证, 处理成字典没有这个问题
+        format_cookie_map = {}
+        for cookie_id, cookie_val in redirect_response.cookies.items():
+            format_cookie_map[cookie_id] = cookie_val
+
+        video_id = redirect_url.split("?")[0].split("/")[-1]
+        post_data = {
+            "fid": "0",
+            "shareResourceType": "PHOTO_OTHER",
+            "shareChannel": "share_copylink",
+            "kpn": "KUAISHOU",
+            "subBiz": "BROWSE_SLIDE_PHOTO",
+            "env": "SHARE_VIEWER_ENV_TX_TRICK",
+            "h5Domain": "m.gifshow.com",
+            "photoId": video_id,
+            "isLongVideo": False,
         }
-        async with httpx.AsyncClient(follow_redirects=True) as client:
-            response = await client.get(share_url, headers=headers)
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                "https://m.gifshow.com/rest/wd/photo/info?kpn=KUAISHOU&captchaToken=",
+                headers={
+                    "Origin": "https://m.gifshow.com",
+                    "Referer": redirect_url,
+                    "Content-Type": "application/json",
+                    "User-Agent": user_agent,
+                },
+                cookies=format_cookie_map,
+                json=post_data,
+            )
+            response.raise_for_status()
 
-        re_pattern = r"window.__APOLLO_STATE__=(.*?);\(function"
-        re_result = re.search(re_pattern, response.text)
+        json_data = response.json()
+        data = json_data["photo"]
 
-        if not re_result or len(re_result.groups()) < 1:
-            raise Exception("failed to parse video JSON info from HTML")
+        # 获取视频地址
+        video_url = ""
+        if "mainMvUrls" in data and len(data["mainMvUrls"]) > 0:
+            video_url = data["mainMvUrls"][0]["url"]
 
-        json_text = re_result.group(1).strip()
-        json_data = json.loads(json_text)
-        video_info = None
-        author_info = None
-        for json_key, json_val in json_data["defaultClient"].items():
-            if json_key.startswith("VisionVideoDetailAuthor"):
-                author_info = json_val
-            elif json_key.startswith("VisionVideoDetailPhoto"):
-                video_info = json_val
-
-        if not video_info or not author_info:
-            raise Exception("failed to parse video JSON info from HTML")
+        # 获取图集
+        ext_params_atlas = data.get("ext_params", {}).get("atlas", {})
+        atlas_cdn_list = ext_params_atlas.get("cdn", [])
+        atlas_list = ext_params_atlas.get("list", [])
+        images = []
+        if len(atlas_cdn_list) > 0 and len(atlas_list) > 0:
+            for atlas in atlas_list:
+                images.append(f"https://{atlas_cdn_list[0]}/{atlas}")
 
         video_info = VideoInfo(
-            video_url=video_info["photoUrl"],
-            cover_url=video_info["coverUrl"],
-            title=video_info["caption"],
+            video_url=video_url,
+            cover_url=data["coverUrls"][0]["url"],
+            title=data["caption"],
             author=VideoAuthor(
-                uid=author_info["id"],
-                name=author_info["name"],
-                avatar=author_info["headerUrl"],
+                uid="",
+                name=data["userName"],
+                avatar=data["headUrl"],
             ),
+            images=images,
         )
         return video_info
 
