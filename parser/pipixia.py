b79ceb3a219438cbb6444cc862af06e16404bad2
wujunwei <1399952803@qq.com>
2024-12-22 21:55:30 +0800

    支持皮皮虾图集;皮皮虾部分有水印视频尝试从comment中获取无水印地址 感谢 @mxccdd 提供comment获取无水印视频解析方法

--- parser/pipixia.py
+++ parser/pipixia.py
@@ -28,14 +28,40 @@ class PiPiXia(BaseParser):
         json_data = response.json()
         if json_data["status_code"] != 0:
             raise Exception(f"获取作品信息失败:prompt={json_data['prompt']}")
-
         data = json_data["data"]["item"]
+
+        author_id = data["author"]["id"]
+
+        # 获取图集图片地址
+        images = []
+        # 如果data含有 images，并且 images 是一个列表
+        if data["note"] is not None:
+            for img in data["note"]["multi_image"]:
+                images.append(img["url_list"][0]["url"])
+
+        video_url = ""
+        if data["video"] is not None:
+            video_url = data["video"]["video_download"]["url_list"][0][
+                "url"
+            ]  # 备用视频地址, 可能有水印
+            # comments中可能带有不带水印视频, 但是comments可能为空
+            for comment in data.get("comments", []):
+                if (
+                    comment["item"]["author"]["id"] == author_id
+                    and comment["item"]["video"]["video_high"]["url_list"][0]["url"]
+                ):
+                    video_url = comment["item"]["video"]["video_high"]["url_list"][0][
+                        "url"
+                    ]
+                    break
+
         video_info = VideoInfo(
-            video_url=data["video"]["video_download"]["url_list"][0]["url"],
+            video_url=video_url,
             cover_url=data["cover"]["url_list"][0]["url"],
             title=data["share"]["title"],
+            images=images,
             author=VideoAuthor(
-                uid=str(data["author"]["id"]),
+                uid=str(author_id),
                 name=data["author"]["name"],
                 avatar=data["author"]["avatar"]["download_list"][0]["url"],
             ),
